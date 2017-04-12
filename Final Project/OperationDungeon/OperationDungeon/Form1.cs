786592998f4c12edbc4fa58ee243f9bc03c16e57
KommonO <kousley@uccs.edu>
2017-04-12 00:26:30 -0600

    Extremely basic Form Completed. No Use Controls just yet

---
+++ Final Project/OperationDungeon/OperationDungeon/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OperationDungeon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Close the Form/Game if the Exit button is clicked
+            this.Close();
+        }
+
+        private void statsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stats stats = new OperationDungeon.Stats();
+            stats.Show();
+        }
+    }
+}
